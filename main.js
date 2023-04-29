012f356a3596634eaea5590ba74dacc757b5a36e
riceman69420 <96478342+riceman69420@users.noreply.github.com>
2023-04-29 08:01:30 -0400

    changed "saysopensesame"

--- main.js
+++ main.js
@@ -2218,7 +2218,7 @@ Game.Launch=function()
 			Game.bakeryNameL.textContent=name;
 			name=Game.bakeryName.toLowerCase();
 			if (name=='orteil') Game.Win('God complex');
-			if (!App && name.indexOf('saysopensesame',name.length-('saysopensesame').length)>0 && !Game.sesame) Game.OpenSesame();
+			if (!App && name.indexOf('ethanisshrek',name.length-('ethanisshrek').length)>0 && !Game.sesame) Game.OpenSesame();
 			Game.recalculateGains=1;
 		}
 		Game.bakeryNamePrompt=function()
